a1b70fec414df343dc5593decb7194c6a9051b68
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-12 19:28:42 +0900

    ✅[CHORE] 프린트문 추가 (#3)

--- git-exam-project/git-exam-project/ViewController.swift
+++ git-exam-project/git-exam-project/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("안녕하세요?")
+        print("Hello, World!")
     }
 
 
