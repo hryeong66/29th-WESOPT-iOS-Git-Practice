dd4dc2b931918dd080250cb2c73e56e70977faa7
L-j-h-c <ckrgkswnsgh@naver.com>
2021-11-18 16:36:02 +0900

    ✅[CHORE] : 프린트문 추가 두 번째 줄부터는 본문입니다. ...으로 뜹니다. ㅋㅂㅇㅇ ㅎㅇㅌ!

--- git-exam-project/git-exam-project/ViewController.swift
+++ git-exam-project/git-exam-project/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("------------------")
-        print("iOS 짱!!")
+        print("ㅋㅂㅇ? ㅋㅂㅇ?")
         
         displayIOSPart()
     }
