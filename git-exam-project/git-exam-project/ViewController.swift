208e2dede7323796cf8cb7ab3e46fad0d2ad8922
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-18 16:34:49 +0900

    ✅[CHORE] ViewController.swift 파일 수정 (#11) print()를 추가했습니다.

--- git-exam-project/git-exam-project/ViewController.swift
+++ git-exam-project/git-exam-project/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("------------------")
-        print("iOS 짱!!")
+        
+        print("아요 짱~~ ^^")
         
         displayIOSPart()
     }
